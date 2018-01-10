8b4e54f8588174b1e03f250f02d5c97abfa955fd
shanhq96 <shanhq96@gmail.com>
2018-01-10 18:59:06 +0800

    remove main method

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,10 +1,5 @@
 public class HelloWorld {
 
-    public static void main(String[] args){
-        HelloWorld helloWorld = new HelloWorld();
-        System.out.println(helloWorld.getHelloWorld());
-    }
-
     public String getHelloWorld(){
         return "hello world";
     }
